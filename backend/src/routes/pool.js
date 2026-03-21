978ea90d2eefddf7f5e49db21a3b5fcd90f66d85
Hetvi4321 <me.hetvi.shah@gmail.com>
2026-03-21 23:49:38 +0530

    bug fixed

--- backend/src/routes/pool.js
+++ backend/src/routes/pool.js
@@ -11,34 +11,33 @@ import {
   getDemoUser
 } from "../controllers/pool.js";
 
-import {verifyJWT} from "../middleware/auth.js"
 const router = Router();
 
 // Route to get a demo user
 router.route("/demo-user").get(getDemoUser);
 
 // Route for driver to create a pool
-router.route("/ready").post(verifyJWT,userIsReadyToPool);
+router.route("/ready").post(userIsReadyToPool);
 
 // Route for passengers to search for available pools
-router.route("/available").get(verifyJWT,checkAvailableForPool);
+router.route("/available").get(checkAvailableForPool);
 
 // Route to get all scheduled pools for general display
-router.route("/all").get(verifyJWT,getAllPools);
+router.route("/all").get(getAllPools);
 
 // Route for driver to view their pools and incoming requests
-router.route("/driver/:driverId").get(verifyJWT,getDriverPools);
+router.route("/driver/:driverId").get(getDriverPools);
 
 // Route for passengers to request joining a pool
-router.route("/request").post(verifyJWT,requestPool);
+router.route("/request").post(requestPool);
 
 // Route for drivers to accept a passenger's request
-router.route("/accept").post(verifyJWT,acceptPool);
+router.route("/accept").post(acceptPool);
 
 // Route for drivers to decline a passenger's request
-router.route("/decline").post(verifyJWT,declinePool);
+router.route("/decline").post(declinePool);
 
 // Route for passenger to check their request status
-router.route("/:poolId/status/:passengerId").get(verifyJWT,poolStatus);
+router.route("/:poolId/status/:passengerId").get(poolStatus);
 
 export default router;
